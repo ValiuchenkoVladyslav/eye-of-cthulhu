c6cf3414dcab1f855d6087ac8d7654f031b25d64
wladyslaw <vladislavvaliuchenkov@gmail.com>
2026-04-15 21:27:15 +0200

    remove polyfills

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 export default {
+   cacheComponents: true,
    experimental: {
       authInterrupts: true,
+      staleTimes: {
+         dynamic: 180,
+      },
+   },
+
+   // https://github.com/vercel/next.js/discussions/64330
+   turbopack: {
+      resolveAlias: {
+         "../build/polyfills/polyfill-module": "./src/empty-polyfill.js",
+         "next/dist/build/polyfills/polyfill-module": "./src/empty-polyfill.js",
+      },
    },
 } satisfies NextConfig;
