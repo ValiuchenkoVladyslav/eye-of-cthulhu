fafffa591f8b1615dd788a997ea86226f0ba758e
wladyslaw <vladislavvaliuchenkov@gmail.com>
2026-04-18 22:11:20 +0200

    remove password from user

--- src/app/(dashboard)/team/actions.ts
+++ src/app/(dashboard)/team/actions.ts
@@ -6,5 +6,9 @@ import { User } from "~/db";
 export async function getMembers() {
    await userAuth();
 
-   return User.getAll().map((user) => ({ ...user }));
+   return User.getAll().map((user) => ({
+      id: user.id,
+      invite: user.inviteCode,
+      username: user.username,
+   }));
 }
